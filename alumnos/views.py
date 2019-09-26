80ebc3de7c94664e4dbdc2ace204efed283939a4
Alejandro <janoli@gmail.com>
2019-09-25 21:55:12 -0300

    segundo commit

--- alumnos/views.py
+++ alumnos/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Alumno
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hola, esta es la aplicacion de alumnos.")
+    latest_alumno_list = Alumno.objects.all()
+    template = loader.get_template('alumnos/index.html')
+    context = {
+            'latest_alumno_list': latest_alumno_list,
+            }
+    return HttpResponse(template.render(context, request))
+
