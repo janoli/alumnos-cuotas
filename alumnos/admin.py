d7b733a862976ec38adbed40da9aece885855459
Alejandro <janoli@gmail.com>
2019-09-27 00:03:35 -0300

    agregadas inscripciones y cursos

--- alumnos/admin.py
+++ alumnos/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Alumno
+from .models import Alumno, Curso, Inscripcion
 # Register your models here.
 
 admin.site.register(Alumno)
+admin.site.register(Curso)
+admin.site.register(Inscripcion)
--- alumnos/models.py
+++ alumnos/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
 # Create your models here.
@@ -9,3 +11,19 @@ class Alumno(models.Model):
 
     def __str__(self):
         return self.apellido_text + ", " + self.nombre_text
+
+class Curso(models.Model):
+    curso_text = models.CharField(max_length=40)
+    año_text = models.CharField(max_length=4)
+    cuota_text = models.IntegerField(null=True)
+    derexam_text = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.curso_text + " (" + self.año_text + ")"
+
+class Inscripcion(models.Model):
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    cursando = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.alumno.apellido_text + ", " + self.alumno.nombre_text + "@" +self.cursando.curso_text
