9067741e9812e9ea70894ee17f9f0b94e9e3ef13
Alejandro <janoli@gmail.com>
2019-10-03 10:49:45 -0300

    agregados varios campos

--- alumnos/models.py
+++ alumnos/models.py
@@ -5,25 +5,34 @@ from django.db import models
 # Create your models here.
 
 class Alumno(models.Model):
-    apellido_text = models.CharField(max_length=40)
-    nombre_text = models.CharField(max_length=40)
-    dni_text = models.CharField(max_length=11)
+    apellidos = models.CharField(max_length=40)
+    nombres = models.CharField(max_length=40)
+    dni = models.CharField(max_length=11)
+    email = models.EmailField(null=True)
+    fecha_inicio = models.DateField(null=True)
+    fecha_nacimiento = models.DateField(null=True)
+    telefono_particular = models.CharField(max_length=40, blank=True)
+    telefono_movil = models.CharField(max_length=40, blank=True)
+    dirección = models.CharField(max_length=40, blank=True)
+    ciudad = models.CharField(max_length=40, blank=True)
+    provincia = models.CharField(max_length=40, blank=True)
+    convenio = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.apellido_text + ", " + self.nombre_text
+        return self.apellidos + ", " + self.nombres
 
 class Curso(models.Model):
-    curso_text = models.CharField(max_length=40)
-    año_text = models.CharField(max_length=4)
-    cuota_text = models.IntegerField(null=True)
-    derexam_text = models.IntegerField(null=True)
+    curso = models.CharField(max_length=40)
+    año = models.CharField(max_length=4)
+    cuota = models.DecimalField(max_digits=12, decimal_places=2)
+    derexam = models.DecimalField(max_digits=12, decimal_places=2)
 
     def __str__(self):
-        return self.curso_text + " (" + self.año_text + ")"
+        return self.curso + " (" + self.año + ")"
 
 class Inscripcion(models.Model):
     alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
     cursando = models.ForeignKey(Curso, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.alumno.apellido_text + ", " + self.alumno.nombre_text + " @ " +self.cursando.curso_text
+        return self.alumno.apellidos + ", " + self.alumno.nombres + " @ " +self.cursando.curso
